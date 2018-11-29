7d018516c22c4d9cb4f27df06acc35476163e254
Jyguy <31291676+Jyguy@users.noreply.github.com>
2018-11-29 15:35:32 -0500

    Fix things

--- Monitors/command-reloader.js
+++ Monitors/command-reloader.js
@@ -2,16 +2,15 @@ const chokidar = require("chokidar");
 const { basename } = require("path");
 const Logger = require("../Monitors/console-monitor.js");
 
-chokidar.watch('./Commands/Developer', { awaitWriteFinish: true }).on('change', (file) => {
-    const commandName = basename(file, ".js")
-    Logger(`Command ${commandName}.js`, "commandupdating");
-    delete require.cache[require.resolve(`../Commands/Developer/${commandName}.js`)];
-    client.commands.delete(`../Commands/Developer/${commandName}.js`);
-    const props = require(`../Commands/Developer/${commandName}.js`);
-    client.commands.set(`../Commands/Developer/${commandName}.js`, props);
-    if (client.commands.set(commandName, props)) {
+module.exports = (client) => {
+    chokidar.watch('./Commands/Developer', { awaitWriteFinish: true }).on("change", (file) => {
+        const commandName = basename(file, ".js")
+        Logger(`Command ${commandName}.js`, "commandupdating");
+        delete require.cache[require.resolve(`../Commands/Developer/${commandName}.js`)];
+        client.commands.delete(`../Commands/Developer/${commandName}.js`);
+        const props = require(`../Commands/Developer/${commandName}.js`);
+        client.commands.set(`../Commands/Developer/${commandName}.js`, props);
+        client.commands.set(commandName, props);
         Logger(`${commandName}.js`, "commandupdate");
-    }
-});
-
-module.exports = chokidar;
+    });
+};
