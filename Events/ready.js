8945130cd907ecdf06102fc469d707975f30d9f1
Jyguy <31291676+Jyguy@users.noreply.github.com>
2018-11-29 15:26:45 -0500

    Extreme Simplifying Janitor Bob

--- Events/ready.js
+++ Events/ready.js
@@ -16,7 +16,7 @@ module.exports = async (client) => {
         ];
 
         setInterval(() => {
-            let ry = Math.floor(Math.random() * presences.length)
+            const ry = Math.floor(Math.random() * presences.length);
 
             client.user.setActivity(presences[ry], {
                 type: "WATCHING"
@@ -37,71 +37,28 @@ module.exports = async (client) => {
         }, 18000);
     }
 
+    const categories = fs.readdirSync('../Commands');
 
-    fs.readdir('./Commands/General', (err, files) => {
-        if (err) console.error(err);
+    categories.forEach(category => {
+        const files = fs.readdirSync(`../Commands/${category}`);
 
-        Logger(`\n${client.user.tag} has loaded with: Total General [Public Use] Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        if (category === 'General') Logger(`\n${client.user.tag} has loaded with: Total General [Public Use] Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        else if (category === 'Music') Logger(`\n${client.user.tag} has loaded with: Total Music Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        else if (category === 'Economy') Logger(`\n${client.user.tag} has loaded with: Total Economy Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        else if (category === 'Moderator') Logger(`\n${client.user.tag} has loaded with: Total Moderator Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        else if (category === 'Administrator') Logger(`\n${client.user.tag} has loaded with: Total Administrator Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
+        else if (category === 'Developer') {
+            Logger(`\n${client.user.tag} has loaded with: Total Developer Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
 
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
+            const readyTime = new Date();
+            const TimeTookToLoad = Math.floor((readyTime - lemonhazeInit) / 1000);
+            console.log(`${client.user.tag} Took ${TimeTookToLoad} second(s) to load everything up.`);
+            if (client.guilds.size === 0) Logger(`\nhttps://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2146958591/`, "noguild");
+        }
+
+        files.forEach(f => {
+            if (f.slice(-2) !== 'js') return;
+            client.commands[f.slice(-2)] = require(`../Commands/${category}/${f}`);
         });
     });
-
-    fs.readdir('./Commands/Music', (err, files) => {
-        if (err) console.error(err);
-
-        Logger(`\n${client.user.tag} has loaded with: Total Music Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
-
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
-        });
-    });
-
-    fs.readdir('./Commands/Economy', (err, files) => {
-        if (err) console.error(err);
-
-        Logger(`\n${client.user.tag} has loaded with: Total Economy Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
-
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
-        });
-    });
-
-    fs.readdir('./Commands/Economy', (err, files) => {
-
-
-        if (err) console.error(err);
-
-        Logger(`\n${client.user.tag} has loaded with: Total Moderator Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
-
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
-        });
-    });
-
-    fs.readdir('./Commands/Administrator', (err, files) => {
-        if (err) console.error(err);
-
-        Logger(`\n${client.user.tag} has loaded with: Total Administrator Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
-
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
-        });
-    });
-
-    fs.readdir('./Commands/Developer/', (err, files) => {
-        if (err) console.error(err);
-
-        Logger(`\n${client.user.tag} has loaded with: Total Developer Commands ${files.length}, Users: ${client.users.size}, Watching: ${client.channels.size} Channels of ${client.guilds.size} guilds.`);
-
-        const readyTime = new Date();
-        const TimeTookToLoad = Math.floor((readyTime - lemonhazeInit) / 1000);
-        console.log(`${client.user.tag} Took ${TimeTookToLoad} second(s) to load everything up.`);
-        if (client.guilds.size === 0) Logger(`\nhttps://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2146958591/`, "noguild");
-
-        files.forEach((f, i) => {
-            if (f.slice(-2) !== "js") {}
-        });
-    });
-}
+};
