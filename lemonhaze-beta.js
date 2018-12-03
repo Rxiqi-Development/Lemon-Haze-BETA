4c8b42deb1973f948ddc17f060568b8584301b0c
Rxiqi-Development <45174623+Rxiqi-Development@users.noreply.github.com>
2018-12-03 17:20:54 +0000

    Add files via upload

--- lemonhaze-beta.js
+++ lemonhaze-beta.js
@@ -5,21 +5,25 @@ const client = new Discord.Client({ fetchAllMembers: true });
 const Logger = require("./Monitors/console-monitor.js"); // Monitoring System for Console.Logs
 
 require("./Monitors/command-reloader.js")(client); // Automated Command Reloader
+require("./Monitors/event-reloader.js")(client); // Automated Event Reloader
 // CONFIGURATION
+
 client.auth = require("./Settings/authentication.json"); // Bot Token, API Keys, Etc
 client.default = require("./Settings/default-options.json"); // Your bots default settings such as prefix.
 client.ascii = require("./Settings/ascii-console.json");// Changable ASCII Font
 client.packages = require("./package.json"); // Required to get Bot's Current Version.
 client.database = require("./Database/sql.js"); // Database Setup & Functions
-client.footer = `Developed By Rxiqi Development • My Prefix: ${client.default.prefix}`; // Footer of each Embed.
+client.footer = `Developed By Rxiqi Development • My Default Prefix: ${client.default.prefix}`; // Footer of each Embed.
 
 // COLLECTERS
 client.commands = new Discord.Collection(); // Collecter for the commands.
+client.aliases = new Discord.Collection();
 
-
-
-
-// ERROR CONTROLLING
+client.commands.set('logs', require('./Commands/Administrator/logs.js'));// Modlog System
+client.commands.set('config', require('./Commands/Administrator/config.js'));// Modlog System
+client.commands.set('eval', require('./Commands/Developer/eval.js'));// Modlog System
+client.commands.set('greeting', require('./Commands/Administrator/greeting.js'));// Modlog System
+// ERROR CONTROLLING client.emit('guildMemberAdd',message.member)
 // client.on("error", (error) => console.error(error));
 // client.on("warn", (warn) => console.warn(warn));
 // client.on("debug", (debug) => console.debug(debug));
@@ -31,6 +35,9 @@ client.on('ready', () => require('./Events/ready.js')(client));
 client.on('message', message => require(`./Events/message.js`)(client, message));
 client.on('messageDelete', message => require('./Events/messageDelete.js')(client, message));
 client.on('messageUpdate', (oldMessage, newMessage) => require('./Events/messageUpdate.js')(client, oldMessage, newMessage));
+
+// GUILD EVENTS
+client.on('guildMemberAdd', member => require('./Events/guildMemberAdd.js')(client, member));
 
 process.on('unhandledRejection', (error) => {
     Logger("[UNHANDLED REJECTION] " + (error.stack == undefined ? error.stack : error.stack), "warn");
