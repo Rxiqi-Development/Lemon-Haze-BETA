651a2499cba0eb8a772be7635737eff609f00b27
Syndicate Corperation <45174623+Rxiqi-Development@users.noreply.github.com>
2018-12-13 01:11:25 +0000

    Version Controller Implemented

--- Database/sql.js
+++ Database/sql.js
@@ -6,151 +6,157 @@ const request = require('snekfetch');
 
 
 try {
-	console.log(`Checking For Latest Version`)
-	let djsVersion = require("../node_modules/discord.js/package.json").version;
-	if (djsVersion !== '11.4.2') {
-		Logger("\nOutdated Discord.JS Version\nPlease Update And Try Again!", "critical")
-		process.exit(1)
-	}
-	let botVersion = require("../package.json").version;
-	if (botVersion !== '1.0.2') {
-		Logger("\nOutdated Lemon Haze BETA Version\nPlease Update From The Github And Try Again!", "critical")
-		process.exit(1)
-	} else {
-		console.log(`Version Up-to-date... \nRunning Bot...`)
-		console.log("Initialized database connection.");
-		const databaseInit = new Date();
-		const tables = {
-			blacklist: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"moderator TEXT NOT NULL",
-				"reason TEXT NOT NULL"
-			],
-			config: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"option TEXT NOT NULL",
-				"value TEXT NOT NULL",
-				"guildID TEXT NOT NULL"
-			],
-			economy: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"userName TEXT NOT NULL",
-				"money INTEGER NOT NULL",
-				"lastDaily TEXT NOT NULL"
-			],
-			register: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"userName TEXT NOT NULL",
-				"registered TEXT NOT NULL"
-			],
-			logs: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"channelID TEXT NOT NULL"
-			],
-			prefix: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"prefix TEXT NOT NULL",
-				"guildID TEXT NOT NULL"
-			],
-			modlog: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"channelID TEXT NOT NULL"
-			],
-			welcome: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"channelID TEXT NOT NULL",
-				"welcome_message TEXT NOT NULL"
-			],
-			muted: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"guildID TEXT NOT NULL"
-			],
-			xp: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"userID TEXT NOT NULL",
-				"exp TEXT NOT NULL",
-				"level TEXT NOT NULL"
-			],
-			toggleLevel: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"bool TEXT NULL"
-			],
-			deleteinvites: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"bool TEXT NULL"
-			],
-			toggleCaptcha: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"bool TEXT NULL"
-			],
-			levelling: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"channelID TEXT NOT NULL"
-			],
-			captcha: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"userID TEXT NOT NULL",
-				"lookingfor TEXT NOT NULL"
-			],
-			captchaset: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"channelID TEXT NOT NULL"
-			],
-			captcharole: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"roleID TEXT NOT NULL"
-			],
-			levels: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"level TEXT NOT NULL"
-			],
-			warnings: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"warnamount TEXT NOT NULL",
-				"warnreason TEXT NOT NULL",
-			],
-			deletelinks: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"guildID TEXT NOT NULL",
-				"bool TEXT NULL"
-			],
-			blacklist: [
-				"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
-				"userID TEXT NOT NULL",
-				"moderator TEXT NOT NULL",
-				"reason TEXT NOT NULL"
-			],
+	console.log(`Checking Latest Version...`)
+	request.get(`http://syndicate-corporation.glitch.me/version`).then(r => {
+		
+		let botVersion = require("../package.json").version;
+		if (botVersion && r.body !== '1.0.2') {
+			console.log("Please go update your bot source from the github here: https://github.com/Rxiqi-Development/Syndicate-Corporation")
+			process.exit(1)
+		}else{
 
-		};
+			
+			let djsVersion = require("../node_modules/discord.js/package.json").version;
+			if (djsVersion !== '11.4.2') {
+				Logger("\nOutdated Discord.JS Version\nPlease Update And Try Again!", "critical")
+				process.exit(1)
+			
+			} else {
+				console.log(`Version Up-to-date... \nRunning Bot...`)
+				console.log("Initialized database connection.");
+				const databaseInit = new Date();
+				const tables = {
+					blacklist: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"moderator TEXT NOT NULL",
+						"reason TEXT NOT NULL"
+					],
+					config: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"option TEXT NOT NULL",
+						"value TEXT NOT NULL",
+						"guildID TEXT NOT NULL"
+					],
+					economy: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"userName TEXT NOT NULL",
+						"money INTEGER NOT NULL",
+						"lastDaily TEXT NOT NULL"
+					],
+					register: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"userName TEXT NOT NULL",
+						"registered TEXT NOT NULL"
+					],
+					logs: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"channelID TEXT NOT NULL"
+					],
+					prefix: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"prefix TEXT NOT NULL",
+						"guildID TEXT NOT NULL"
+					],
+					modlog: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"channelID TEXT NOT NULL"
+					],
+					welcome: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"channelID TEXT NOT NULL",
+						"welcome_message TEXT NOT NULL"
+					],
+					muted: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"guildID TEXT NOT NULL"
+					],
+					xp: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"userID TEXT NOT NULL",
+						"exp TEXT NOT NULL",
+						"level TEXT NOT NULL"
+					],
+					toggleLevel: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"bool TEXT NULL"
+					],
+					deleteinvites: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"bool TEXT NULL"
+					],
+					toggleCaptcha: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"bool TEXT NULL"
+					],
+					levelling: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"channelID TEXT NOT NULL"
+					],
+					captcha: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"userID TEXT NOT NULL",
+						"lookingfor TEXT NOT NULL"
+					],
+					captchaset: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"channelID TEXT NOT NULL"
+					],
+					captcharole: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"roleID TEXT NOT NULL"
+					],
+					levels: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"level TEXT NOT NULL"
+					],
+					warnings: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"warnamount TEXT NOT NULL",
+						"warnreason TEXT NOT NULL",
+					],
+					deletelinks: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"guildID TEXT NOT NULL",
+						"bool TEXT NULL"
+					],
+					blacklist: [
+						"id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL",
+						"userID TEXT NOT NULL",
+						"moderator TEXT NOT NULL",
+						"reason TEXT NOT NULL"
+					],
 
-		for (let table in tables) {
+				};
 
-			database.run(`CREATE TABLE ${table} (${tables[table].join(", ")})`, () => {
-				const readyTime = new Date();
-				const TimeTookToLoad = Math.floor((readyTime - databaseInit) / 1000);
-				console.log(`Database Took ${TimeTookToLoad} second(s) to load table ${table}.`);
-			});
+				for (let table in tables) {
+
+					database.run(`CREATE TABLE ${table} (${tables[table].join(", ")})`, () => {
+						const readyTime = new Date();
+						const TimeTookToLoad = Math.floor((readyTime - databaseInit) / 1000);
+						console.log(`Database Took ${TimeTookToLoad} second(s) to load table ${table}.`);
+					});
+				}
+			}
 		}
-	}
-
+	})
 
 
 
